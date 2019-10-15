c89239b4584e79954cc0879a5b248a01622b1150
unknown <phuocduc2012@gmail.com>
2019-10-15 17:03:24 +0700

    react game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
+import ChoiceButtons from "./modules/ChoiceButton";
+import { CHOICES, getRoundOutcome } from "./utils";
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ChoiceCard from './modules/ChoiceCard'
 
-function App() {
+
+
+function Main() {
+  
+  const [prompt, setGamePrompt] = useState('--');
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  // console.log('123',[computerChoice])
+  const [previousWinner, setPreviousWinner] = useState(null);
+
+  const [gameHistory, setGameHistory] = useState([])
+
+  
+  // console.log('history',[gameHistory,setGameHistory])
+
+  const onPlayerChoose = playerChoice =>{
+ 
+
+    const [result,comChoice] = getRoundOutcome(playerChoice)
+    const newUserChoice = CHOICES[playerChoice];
+    const newComputerChoice = CHOICES[comChoice];
+
+    if (result === "Victory!") {
+      setPreviousWinner("You");
+    } else if (result === "Defeat!") {
+      setPreviousWinner("Computer");
+    } else {
+      setPreviousWinner("Tie");
+    }
+   
+
+  setPlayerChoice(newUserChoice);
+  setComputerChoice(newComputerChoice);
+
+  setGamePrompt(result);
+  
+  gameHistory.push(result)
+  setGameHistory(gameHistory);
+  
+}
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row mb-3">
+          <div className="col-md-8 themed-grid-col"></div>
+          <h1>{prompt}</h1>
+          <ChoiceCard title="Computer" winner={true} previousWinner={previousWinner} imgURL={computerChoice && computerChoice.url}/>
+         
+          <ChoiceCard title="You" winner={false} previousWinner={previousWinner} imgURL={playerChoice && playerChoice.url}/>
+          <ChoiceButtons onPlayerChoose={onPlayerChoose} />
+        </div>
+        <div className="col-md-4 themed-grid-col">
+  <h3>History</h3>
+  <ul>
+    {gameHistory.map(result => {
+      return <li>{result}</li>;
+    })}
+  </ul>
+</div>
+      </div>
     </div>
   );
 }
 
-export default App;
+// function App() {
+//   const [isSignIn,setSignedIn] = useState(false)
+//   return ( 
+//   //  <Main />
+//     <div>sdfesfs</div>
+//   )
+// }
+
+export default Main;
---
+++ src/modules/ChoiceButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function ChoiceButtons(props) {
+    console.log('props',props)
+  return (
+    <div className="container">
+             <button className='btn btn-success btn-lg' onClick={()=>props.onPlayerChoose("rock")}>
+                Rock
+              </button>
+              <button className='btn btn-success btn-lg' onClick={()=>props.onPlayerChoose("paper")}>
+                Paper
+              </button>
+              <button className='btn btn-success btn-lg' onClick={()=>props.onPlayerChoose("scissors")}>
+                Scissors
+            </button>
+          </div>
+  );
+}
